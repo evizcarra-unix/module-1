01654b0123f4f5555af03a194027714ffc294056
Ezra Vizcarra <vizcarraezra@yahoo.com>
2021-04-21 09:45:04 -0600

    Uploads simple program and updates the README file.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# module-1
-CSE 310
+# **W01 Prove: Create Personal Software Portfolio**
+##CSE 310: Applied Programming
+Purpose:
+    1. Using Visual Studio Code, write a very simple "Hello World" program in any programming language.
+    2. Provide a YouTube link to a video about the software you created. Create a one minute video that provides a demo of your software and walkthrough of the code.
+    3. Publish your code (with the README.md) into a public GitHub repository.
---
+++ Simple Program.py
@@ -0,0 +1,29 @@
+"""
+Author: Ezra Vizcarra
+Topic: Simple Program
+Purpose: Nothing too serious with this program. This is to showcase and verify the synchronized process
+         with VS Code and GitHub."""
+
+#Defined functions for each set {regular, uppercase, lowercase}.
+def display_regular(message):
+    print(message)
+
+def display_uppercase(message):
+    new_message = message.upper()
+    print(new_message)
+
+def display_lowercase(message):
+    new_message = message.lower()
+    print(new_message)
+
+#Visual welcome screen to User with a brief intro.
+print('----Hello World!----i\nThis is a simple testing sentence formatter:\n'
+'   1. Prints regular\n   2. Prints uppercase\n   3.Prints lowercase')
+
+#Skips a line to give it a space between both prints, this requires User to type something.
+user_message = input('\nWhat would you like to test? ')
+
+#Calls the functions and prints them as listed on intro in line 21.
+display_regular(user_message)
+display_uppercase(user_message)
+display_lowercase(user_message)
